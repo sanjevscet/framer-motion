7f4a5ab4570c46d8de3add612939bc769a277994
Sanjeev Kumar <Sanjeev.kumar@goavege.com>
2020-08-05 22:58:22 +0530

    Lesson 7 starting styling by variants

--- pizzajoint/src/components/Base.js
+++ pizzajoint/src/components/Base.js
@@ -1,14 +1,49 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion'
+
+const containerVariants = {
+  hidden:{
+    opacity: 0,
+    x:'100vw',
+  },
+  visible:{
+    opacity: 1,
+    x:0,
+    transition: {
+      type: 'spring',
+      delay: 0.5
+    }
+
+  }
+}
+
+const nextVariant = {
+  hidden:{
+    x:'-100vw',
+    opacity: 0,
+  },
+  visible:{
+    x: 0,
+    opacity: 1,
+
+    transition: {
+      type:'spring',
+      stiffness: 120,
+    }
+  }
+
+}
+
+
 const Base = ({ addBase, pizza }) => {
   const bases = ['Classic', 'Thin & Crispy', 'Thick Crust'];
 
   return (
     <motion.div className="base container"
-      initial={{ x: '100vw' }}
-      animate={{ x: 0 }}
-      transition={{ type: 'spring', delay: 0.5 }}
+    variants={containerVariants}
+    initial="hidden"
+    animate="visible"
     >
 
       <h3>Step 1: Choose Your Base</h3>
@@ -28,9 +63,7 @@ const Base = ({ addBase, pizza }) => {
 
       {pizza.base && (
         <motion.div className="next"
-          initial={{ x: '-100vw' }}
-          animate={{ x: 0 }}
-          transition={{ type: 'spring', stiffness: 120 }}
+        variants={nextVariant}
         >
           <Link to="/toppings">
             <motion.button whileHover={{
